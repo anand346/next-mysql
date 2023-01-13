9db9932419708888813f73deaf5c47201ffbaed1
= <=>
2023-01-14 00:54:41 +0530

    compiled app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SERVER: process.env.SERVER,
-    DATABASE_URL: process.env.DATABASE_URL
-  },
 }
 
 module.exports = nextConfig
